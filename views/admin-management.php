84f6b962ca89f9076f2df693c304ec2c72827707
Duane Cilliers <duane@Duanes-MacBook-Air.local>
2013-01-20 12:23:57 +0200

    Created options and management views and integrated errors and warnings

---
+++ views/admin-management.php
@@ -0,0 +1,14 @@
+<div class="wrap">
+	<div id="icon-tools" class="icon32"><br></div>
+	<h2>Gravity Forms to Solve360 Export Mangement</h2>
+</div>
+
+<?php
+	if ( $this->errors ) {
+		echo $this->errors;
+		return;
+	}
+	if ( $this->warnings ) {
+		echo $this->warnings;
+	}
+?>
